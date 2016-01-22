cc4923d6dac3e0dac7392ece2c9647f660c5ae59
ababilone <arnaud.babilone@gmail.com>
2016-01-22 21:20:33 +0100

    Add MaxManager.Web.Test project

---
+++ MaxManager.Web.Test/Lan/Parser/HMessageParserTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MaxManager.Web.Test.Lan.Parser
+{
+	[TestClass]
+	public class HMessageParserTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ MaxManager.Web.Test/Lan/Parser/MMessageParserTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace MaxManager.Web.Test.Lan.Parser
+{
+	[TestClass]
+	public class MMessageParserTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
