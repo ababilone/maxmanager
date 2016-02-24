0b1f47fa6ad10a55be93c3dbb579dbb2981370c2
ababilone <arnaud.babilone@gmail.com>
2016-02-24 08:15:59 +0100

    Settings page layout

--- MaxManager/ValueConverters/BooleanToVisibilityConverter.cs
+++ MaxManager/ValueConverters/BooleanToVisibilityConverter.cs
@@ -9,14 +9,17 @@ namespace MaxManager.ValueConverters
 		public object Convert(object value, Type targetType, object parameter, string language)
 		{
 			if (value == null)
-				return Visibility.Collapsed;
+				return parameter == null ? Visibility.Collapsed : Visibility.Visible;
 
-			return System.Convert.ToBoolean(value) ? Visibility.Visible : Visibility.Collapsed;
+			if (System.Convert.ToBoolean(value))
+				return parameter == null ? Visibility.Visible : Visibility.Collapsed;
+
+			return parameter == null ? Visibility.Collapsed : Visibility.Visible;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, string language)
 		{
-			if (value is Visibility && (Visibility) value == Visibility.Visible)
+			if (value is Visibility && (Visibility)value == Visibility.Visible)
 				return true;
 
 			return false;
--- MaxManager/ViewModels/SettingsViewModel.cs
+++ MaxManager/ViewModels/SettingsViewModel.cs
@@ -5,13 +5,26 @@ namespace MaxManager.ViewModels
 {
 	public class SettingsViewModel : ViewModelBase
 	{
+		public bool IsDiscoveryEnabled
+		{
+			get { return _isDiscoveryEnabled; }
+			set { Set(ref _isDiscoveryEnabled, value); }
+		}
+		private bool _isDiscoveryEnabled;
+
 		public int DiscoveryTimeOut
 		{
 			get { return _discoveryTimeOut; }
 			set { Set(ref _discoveryTimeOut, value); }
 		}
-
 		private int _discoveryTimeOut;
+
+		public string CubeAddress
+		{
+			get { return _cubeAddress; }
+			set { Set(ref _cubeAddress, value); }
+		}
+		private string _cubeAddress;
 
 		public bool IsDebugEnabled
 		{
