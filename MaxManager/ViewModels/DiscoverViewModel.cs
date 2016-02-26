5b86863bc572a30c73a935453345c0a355dbc659
ababilone <arnaud.babilone@gmail.com>
2016-02-26 22:11:31 +0100

    Link DiscoverViewModel & SettingsService

--- MaxManager/ViewModels/DiscoverViewModel.cs
+++ MaxManager/ViewModels/DiscoverViewModel.cs
@@ -4,6 +4,7 @@ using Windows.Networking;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Views;
 using MaxManager.Model;
+using MaxManager.Services.Settings;
 using MaxManager.Utils.Timer;
 using MaxManager.ViewModels.Common;
 using MaxManager.Web.Lan.Discovery;
@@ -14,12 +15,14 @@ namespace MaxManager.ViewModels
 	{
 		private readonly IMaxCubeDiscoverer _maxCubeDiscoverer;
 		private readonly INavigationService _navigationService;
+		private readonly ISettingService _settingService;
 		private readonly ConcurrentDictionary<HostName, CubeInfo> _cubes;
 
-		public DiscoverViewModel(IMaxCubeDiscoverer maxCubeDiscoverer, INavigationService navigationService)
+		public DiscoverViewModel(IMaxCubeDiscoverer maxCubeDiscoverer, INavigationService navigationService, ISettingService settingService)
 		{
 			_maxCubeDiscoverer = maxCubeDiscoverer;
 			_navigationService = navigationService;
+			_settingService = settingService;
 
 			_cubes = new ConcurrentDictionary<HostName, CubeInfo>();
 
@@ -28,54 +31,65 @@ namespace MaxManager.ViewModels
 				_cubes.TryAdd(cubeDiscoveredEventArgs.RemoteAddress, cubeDiscoveredEventArgs.CubeInfo);
 			};
 
-		Progress = new Progress();
-	}
+			Progress = new Progress();
+		}
 
-	public Progress Progress { get; }
+		public Progress Progress { get; }
 
-	private async void Discover()
-	{
-		Progress.IsEnabled = true;
-		Progress.PrimaryMessage = "Discovering Max! Cube on network...";
-
-		_cubes.Clear();
-
-		await _maxCubeDiscoverer.DiscoverCubes();
-
-		var countdownTimer = new CountdownTimer();
-		countdownTimer.CountdownSecondElasped += (sender, seconds) =>
+		private async void Discover()
 		{
-			Progress.SecondaryMessage = string.Format("{0}s remaining", seconds);
-		};
-		countdownTimer.CountdownFinished += sender =>
-		{
-			var hostNames = string.Join(",", _cubes.Keys.Select(name => name.DisplayName));
-			Progress.PrimaryMessage = string.Format("Discovered {0} Max! Cube{1} @ {2}", _cubes.Count, _cubes.Count > 1 ? "s" : "", hostNames);
-			Progress.SecondaryMessage = "";
-			Progress.IsEnabled = false;
+			Progress.IsEnabled = true;
+			Progress.PrimaryMessage = "Discovering Max! Cube on network...";
 
-			if (_cubes.Count == 1)
+			_cubes.Clear();
+
+			await _maxCubeDiscoverer.DiscoverCubes();
+
+			var countdownTimer = new CountdownTimer();
+			countdownTimer.CountdownSecondElasped += (sender, seconds) =>
 			{
-				var keyValuePair = _cubes.FirstOrDefault();
+				Progress.SecondaryMessage = string.Format("{0}s remaining", seconds);
+			};
+			countdownTimer.CountdownFinished += sender =>
+			{
+				var hostNames = string.Join(",", _cubes.Keys.Select(name => name.DisplayName));
+				Progress.PrimaryMessage = string.Format("Discovered {0} Max! Cube{1} @ {2}", _cubes.Count, _cubes.Count > 1 ? "s" : "", hostNames);
+				Progress.SecondaryMessage = "";
+				Progress.IsEnabled = false;
+
+				if (_cubes.Count == 1)
+				{
+					var keyValuePair = _cubes.FirstOrDefault();
+					var cubeInfoWithHostName = new CubeInfoWithHostName
+					{
+						CubeInfo = keyValuePair.Value,
+						HostName = keyValuePair.Key
+					};
+
+					_navigationService.NavigateTo(NavigationKeys.Home, cubeInfoWithHostName);
+				}
+			};
+			countdownTimer.Start(_settingService.DiscoveryTimeOut);
+		}
+
+		public void Activate(object parameter)
+		{
+			if (_settingService.IsDiscoveryEnabled)
+			{
+				Discover();
+			}
+			else if (!string.IsNullOrEmpty(_settingService.CubeAddress))
+			{
 				var cubeInfoWithHostName = new CubeInfoWithHostName
 				{
-					CubeInfo = keyValuePair.Value,
-					HostName = keyValuePair.Key
+					HostName = new HostName(_settingService.CubeAddress)
 				};
-
 				_navigationService.NavigateTo(NavigationKeys.Home, cubeInfoWithHostName);
 			}
-		};
-		countdownTimer.Start(1);
-	}
+		}
 
-	public void Activate(object parameter)
-	{
-		Discover();
+		public void Deactivate(object parameter)
+		{
+		}
 	}
-
-	public void Deactivate(object parameter)
-	{
-	}
-}
 }
